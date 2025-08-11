23fd97ded1f3d551693d546d161142450478bc11
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-08-11 14:12:13 -0700

    Create main.swift

---
+++ life/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  
+//
+//  Created by William Chuter-Davies on 8/11/25.
+//
+
+let engine = Engine()
+let initialLiveCells: Set<Cell> = [
+    Cell(x: 0, y: 0)
+]
+
+engine.setLiveCells(to: initialLiveCells)
